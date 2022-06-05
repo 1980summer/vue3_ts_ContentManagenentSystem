9bbd3103246e247c8ee3a52dc15723356279c4cf
1980summer <lyuzuru@foxmail.com>
2022-06-05 18:14:52 +0800

    ci: 2-配置vuex

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
     <router-link to="/login">Login</router-link>
     <router-link to="/main">Main</router-link>
     <router-view></router-view>
+
+    <h2>{{ $store.state.name }}</h2>
   </div>
 </template>
 
