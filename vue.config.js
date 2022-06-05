5580eef5f7bd8ce5f2621bddf77c6065f60699a6
1980summer <lyuzuru@foxmail.com>
2022-06-05 16:53:59 +0800

    ci: 增添了一些搭建项目的配置

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,30 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const path = require('path')
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
 
+module.exports = {
+  outputDir: './build',
+  //   configureWebpack: {
+  //     // resolve 用于配置路径
+  //     resolve: {
+  //       alias: {
+  //         components: '@/component'
+  //       }
+  //     }
+  //   },
+  // configureWebpack: (config) => {
+  //   config.resolve.alias = {
+  //     '@': path.resolve(__dirname, 'src'),
+  //     components: '@/components'
+  //   }
+  // }
 
+  // 3.配置方式三
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', path.resolve(__dirname, 'src'))
+      .set('components', '@/components')
+  }
+}
