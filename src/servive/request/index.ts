6cf0be59c378e8220b2183e9045966f93e155277
1980summer <lyuzuru@foxmail.com>
2022-06-07 18:10:37 +0800

    feat: 5-8_Axios_单个请求拦截的类型问题

--- src/servive/request/index.ts
+++ src/servive/request/index.ts
@@ -83,7 +83,7 @@ class YXRequest {
 
   // 封装一个request方法，用来发送请求，以供外界调用
   // 单个请求拦截器的封装
-  request<T>(config: YXRequestConfig): Promise<T> {
+  request<T>(config: YXRequestConfig<T>): Promise<T> {
     return new Promise((resolve, reject) => {
       // 1 单个请求对config的处理
       if (config.interceptors?.requestInterceptor) {
@@ -119,20 +119,20 @@ class YXRequest {
   }
 
   // 封装
-  get<T>(config: YXRequestConfig): Promise<T> {
+  get<T>(config: YXRequestConfig<T>): Promise<T> {
     // 调用request
     return this.request<T>({ ...config, method: 'GET' })
   }
 
-  post<T>(config: YXRequestConfig): Promise<T> {
+  post<T>(config: YXRequestConfig<T>): Promise<T> {
     return this.request<T>({ ...config, method: 'POST' })
   }
 
-  delete<T>(config: YXRequestConfig): Promise<T> {
+  delete<T>(config: YXRequestConfig<T>): Promise<T> {
     return this.request<T>({ ...config, method: 'DELETE' })
   }
 
-  patch<T>(config: YXRequestConfig): Promise<T> {
+  patch<T>(config: YXRequestConfig<T>): Promise<T> {
     return this.request<T>({ ...config, method: 'PATCH' })
   }
 }
--- src/servive/request/types.ts
+++ src/servive/request/types.ts
@@ -1,12 +1,12 @@
 import { AxiosRequestConfig, AxiosResponse } from 'axios'
-export interface YXRequestInterceptors {
+export interface YXRequestInterceptors<T = AxiosResponse> {
   requestInterceptor?: (config: AxiosRequestConfig) => AxiosRequestConfig
   requestInterceptorCatch?: (error: any) => any
-  responseInterceptor?: (config: any) => any
+  responseInterceptor?: (config: T) => T
   responseInterceptorCatch?: (error: any) => any
 }
 
-export interface YXRequestConfig extends AxiosRequestConfig {
-  interceptors?: YXRequestInterceptors
+export interface YXRequestConfig<T = AxiosResponse> extends AxiosRequestConfig {
+  interceptors?: YXRequestInterceptors<T>
   showLoding?: boolean
 }
