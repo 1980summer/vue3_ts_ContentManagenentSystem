e29566f8791a1522029be4afcb700e25791b4b22
1980summer <lyuzuru@foxmail.com>
2022-06-09 01:24:25 +0800

    feat: 8_首页_用户头像和姓名展示

--- src/components/nav-header/src/nav-header.vue
+++ src/components/nav-header/src/nav-header.vue
@@ -3,17 +3,22 @@
     <el-icon class="fold-menu"
       ><component :is="isFold ? 'Fold' : 'Expand'" @click="handleFoldClick"></component
     ></el-icon>
+    <div class="content">
+      <div class="div">面包屑</div>
+      <UserInfo></UserInfo>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
 import { Fold, Expand } from '@element-plus/icons'
-
+import UserInfo from './user-info.vue'
 export default defineComponent({
   components: {
     Fold,
-    Expand
+    Expand,
+    UserInfo
   },
   emits: ['foldChange'],
   setup(props, { emit }) {
@@ -29,9 +34,18 @@ export default defineComponent({
 
 <style scoped lang="less">
 .nav-header {
+  display: flex;
+  width: 100%;
   .fold-menu {
     font-size: 30px;
     cursor: pointer;
+  }
+  .content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex: 1;
+    padding: 0 20px;
   }
 }
 </style>
---
+++ src/components/nav-header/src/user-info.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="user-info">
+    <el-dropdown>
+      <span class="el-dropdown-link">
+        <span>{{ name }}</span>
+        <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" />
+      </span>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <el-dropdown-item>
+            <el-icon><Close /></el-icon>
+            退出登录</el-dropdown-item
+          >
+          <el-dropdown-item divided>
+            <el-icon><Document /></el-icon>
+            用户信息</el-dropdown-item
+          >
+          <el-dropdown-item>
+            <el-icon><Setting /></el-icon>
+            系统管理</el-dropdown-item
+          >
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue'
+import { useStore } from '@/store'
+import { Close, Document, Setting } from '@element-plus/icons'
+
+export default defineComponent({
+  components: {
+    Close,
+    Document,
+    Setting
+  },
+  setup() {
+    const store = useStore()
+    const name = computed(() => store.state.loginModule.userInfo.name)
+    return { name }
+  }
+})
+</script>
+
+<style scoped lang="less">
+.el-dropdown-link {
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+}
+</style>
