3fe0008ae4ae70534d54c9c307f997a1c6303e1f
1980summer <lyuzuru@foxmail.com>
2022-06-09 21:52:49 +0800

    feat: eltable展示数据和YxTable的封装

--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -2,24 +2,31 @@
   <div class="user">
     <div class="search"></div>
     <PageSearch :searchFormConfig="searchFormConfig"></PageSearch>
-    <div class="content"></div>
+
+    <div class="content">
+      <YxTable :listData="useList" :propList="propList"></YxTable>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import { useStore } from 'vuex'
+import { defineComponent, computed } from 'vue'
+import { useStore } from '@/store'
 import PageSearch from '@/components/page-search'
+import YxTable from '@/base-ui/table'
 
 import { searchFormConfig } from './config/serach.config'
 
 export default defineComponent({
   name: 'user',
   components: {
-    PageSearch
+    PageSearch,
+    YxTable
   },
   setup() {
     const store = useStore()
+
+    // 发送请求
     store.dispatch('systemModule/getPageListAction', {
       pageUrl: 'users/list',
       queryInfo: {
@@ -27,9 +34,27 @@ export default defineComponent({
         size: 10
       }
     })
-    return { searchFormConfig }
+
+    const useList = computed(() => store.state.systemModule.userList)
+    const userCount = computed(() => store.state.systemModule.userCount)
+
+    const propList = [
+      { prop: 'name', label: '用户名', minWidth: '100' },
+      { prop: 'realname', label: '真实姓名', minWidth: '100' },
+      { prop: 'cellphone', label: '手机号码', minWidth: '100' },
+      { prop: 'enable', label: '状态', minWidth: '100' },
+      { prop: 'createAt', label: '创建时间', minWidth: '250' },
+      { prop: 'updateAt', label: '更新时间', minWidth: '250' }
+    ]
+
+    return { searchFormConfig, useList, propList }
   }
 })
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.content {
+  padding: 40px;
+  border-top: solid 1px #f5f5f5;
+}
+</style>
