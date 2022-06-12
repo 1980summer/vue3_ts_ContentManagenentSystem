60574b3e6742c8f6ab19c1f8e2998e1469cee227
1980summer <lyuzuru@foxmail.com>
2022-06-13 02:53:23 +0800

    feat: 对useEachart的封装

---
+++ src/base-ui/echart/hooks/useEchart.ts
@@ -0,0 +1,24 @@
+import * as echarts from 'echarts'
+
+export default function (el: HTMLElement) {
+  const echartInstance = echarts.init(el)
+
+  const setOptions = (options: echarts.EChartsOption) => {
+    echartInstance.setOption(options)
+  }
+
+  const updateSize = () => {
+    echartInstance.resize()
+  }
+  // 监听浏览器c尺寸发生变化， 使图标有响应式效果
+  window.addEventListener('resize', () => {
+    echartInstance.resize()
+  })
+
+  return {
+    echartInstance,
+    setOptions,
+    updateSize
+  }
+}
+//
